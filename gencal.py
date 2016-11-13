a22ce94c2c63a2dab31c7b0fe083a86ec1b6dedb
Ue-Li Pen <pen@cita.utoronto.ca>
2016-11-13 16:11:50 -0500

    works for LA

---
+++ gencal.py
@@ -0,0 +1,12 @@
+import astropy.io.fits as fits
+import numpy as np
+from pylab import *
+import matplotlib.pylab as plt
+
+nt=2173
+ipol=0
+for antenna in ['fd','la'] :
+    (w,omega,ft)=np.load('J1751modelsvd'+antenna+'ypol'+str(ipol)+'.npy')
+    times=np.load('times.npy')#np.load('time'+antenna+'y.npy')
+    tmodel=ft*exp((0.+1.j)*times[:nt,np.newaxis]*omega)*w
+    np.save('J1751model'+antenna+'ypol'+str(ipol)+'.npy',tmodel)
